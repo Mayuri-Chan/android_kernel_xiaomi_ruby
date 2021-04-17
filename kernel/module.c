7f9509d95b4aa8f33181e18a49a938b747787a22
raymanfx <raymanfx@gmail.com>
2021-04-17 21:40:33 +0200

    kernel: module: ignore all magic mismatches
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- kernel/module.c
+++ kernel/module.c
@@ -3760,6 +3760,8 @@ static int load_module(struct load_info *info, const char __user *uargs,
 		err = -EPERM;
 		goto free_copy;
 	}
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 
 	err = module_sig_check(info, flags);
 	if (err)
